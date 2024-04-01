8cec312d7ae233ebe9554a37c8a90872f17b1130
Satyaki Ghosh <satyakighosh594@gmail.com>
2024-04-01 10:24:40 +0530

    Adding asserts for throws and timeout

---
+++ Junit-Development-Sample-ThrowsAndTimeouts/src/test/java/com/practice/JUnit/Junit_Development_Sample/TestCases/CustomTests.java
@@ -0,0 +1,89 @@
+package com.practice.JUnit.Junit_Development_Sample.TestCases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import com.practice.JUnit.Junit_Development_Sample.Utility.SampleMethods;
+
+@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
+class CustomTests 
+{
+	SampleMethods obj = null;
+	
+	@BeforeAll
+	public static void setUpBeforeAll()
+	{
+		System.out.println("In the Before All method");
+	}
+	
+	@AfterAll
+	public static void setUpAfterAll()
+	{
+		System.out.println("In the After all method");
+	}
+	
+	@BeforeEach
+	public void setUpBeforeEach()
+	{
+		System.out.println("In the Before Each method");
+		obj = new SampleMethods();
+	}
+	
+	@AfterEach
+	public void setUpAfterEach()
+	{
+		System.out.println("In the After each method");
+	}
+	
+	@Test
+	//@DisplayName("Test Equal or Not Equals")
+	public void testEqualsOrNotEquals()
+	{
+		
+		System.out.println("In the assert equals/notequals method");
+		Assertions.assertEquals(8, obj.add(4,4),"Assert equal Test Case failed");
+		//Assertions.assertEquals(8, obj.add(2,4),"Assert equals Test Case failed");
+		
+		Assertions.assertNotEquals(6, obj.add(4, 4), "Assert not equal test failed");
+		//Assertions.assertNotEquals(6, obj.add(2, 4),"Assert not equal test failed");
+	}
+	
+	@Test
+	//@DisplayName("Test Null or Not Null")
+	public void testNullOrNotNull()
+	{
+		System.out.println("In the assert null/not null method");
+		Assertions.assertNull(obj.testString(null), "Objecvt shoudl be null");
+		//Assertions.assertNull(obj.testString("Satyaki Ghosh"), "Object should be null");
+		
+		Assertions.assertNotNull(obj.testString("Satyaki Ghosh"), "Object shoudl not be null");
+		//Assertions.assertNotNull(obj.testString(null), "Object should not be null");
+	}
+	
+	@Test
+	public void testThrows()
+	{
+		System.out.println("In the test throws method");
+		Assertions.assertThrows(Exception.class, ()->{obj.testException(-1);}, "Throws Exception");
+		Assertions.assertDoesNotThrow(()->{obj.testException(10);});
+	}
+	
+	@Test
+	public void testTimeouts()
+	{
+		System.out.println("Testing the assert time out method");
+		Assertions.assertTimeoutPreemptively(Duration.ofMillis(4000), ()->{obj.testTimeouts();});
+	}
+
+}
